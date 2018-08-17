1c713a77af85294421e95ab49cedc0be95005362
Zahid A. Chowdhury <zahid@iotek.fi>
2018-08-17 13:12:25 +0300

    js-lesson-13- inheritance- added bind keyword

--- objectInherit.js
+++ objectInherit.js
@@ -8,5 +8,10 @@ sound:'meeeeaw',
 
 cat.talk();
 
-var tap = cat.talk 
-tap.talk()
+//var tap = cat.talk // undefined. should be used bind 
+// bind will copy entire object properties to the attached 
+// object 
+var bindFunction = bindFunction.bind ( cat)
+
+bindFunction.talk()
+

22570d4522250d30bd19b7bf262d43f2ce848872
Zahid A. Chowdhury <zahid@iotek.fi>
2018-08-20 13:01:40 +0300

    implecit explecit bind

--- objectInherit.js
+++ objectInherit.js
@@ -1,17 +1,25 @@
-var cat  = {
-sound:'meeeeaw',
-    talk:function(){
-        console.log ('catsasy',this.sound)
+var customer  = {
+    cash:'Euro',
+    bank:function(){
+        console.log ('Customer Currency: ',this.cash)
     }
 
 }
 
-cat.talk();
-
-//var tap = cat.talk // undefined. should be used bind 
-// bind will copy entire object properties to the attached 
-// object 
-var bindFunction = bindFunction.bind ( cat)
-
-bindFunction.talk()
+function product( name ){
+    // cnstructor function
+    console.log('Product name:'+name+' '+'bought with:'+this.cash);
+}
+customer.bank(); // output> Customer Currency:  Euro
+/*
+function.prototype.call()
+function.prototype.bind()
+function.prototype.apply()
+*/
+// explicit binding 
+var newCustomer = customer.bank // undefined. should be used bind 
+console.dir ( newCustomer); // output> [function:bank]
+// bind will copy entire object properties to the target function / object 
+var bindCustomer = product.bind(customer)
+bindCustomer ('cocaCola');
 

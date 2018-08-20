33308c9ca00f352878064348a955f47bebb83696
Zahid A. Chowdhury <zahid@iotek.fi>
2018-08-20 13:07:10 +0300

    bind and call explained in one line with 4 words

--- objectInherit.js
+++ objectInherit.js
@@ -19,7 +19,12 @@ function.prototype.apply()
 // explicit binding 
 var newCustomer = customer.bank // undefined. should be used bind 
 console.dir ( newCustomer); // output> [function:bank]
-// bind will copy entire object properties to the target function / object 
+
+/*
+bind is same as function.prototype.call() , but just takes a parameter for the target objet,
+bind will copy entire object properties to the target function / object 
+and wll not execute the function , it will assign to a new variable 
+*/
 var bindCustomer = product.bind(customer)
 bindCustomer ('cocaCola');
 
